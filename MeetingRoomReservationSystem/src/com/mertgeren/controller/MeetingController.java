b453b28b0d586ff08e4746967389f06c21ed0caf
mgeren <mgeren@mertgeren.sni.local>
2019-07-22 11:58:25 +0300

    listMeetings method is updated

--- MeetingRoomReservationSystem/src/com/mertgeren/controller/MeetingController.java
+++ MeetingRoomReservationSystem/src/com/mertgeren/controller/MeetingController.java
@@ -1,15 +1,31 @@
 package com.mertgeren.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mertgeren.dao.MeetingDAO;
+import com.mertgeren.entity.Meeting;
 
 @Controller
 @RequestMapping("/meeting")
 public class MeetingController {
 	
+	// need to inject the meeting dao
+	@Autowired
+	private MeetingDAO meetingDAO;
+
 	@RequestMapping("/list")
-	public String listDetails(Model theModel) {
+	public String listMeetings(Model theModel) {
+		
+		// get meetings from the dao
+		List<Meeting> theMeetings = meetingDAO.getMeetings();
+		
+		// add the meetings to the model
+		theModel.addAttribute("meetings", theMeetings);					// name - value
 		
 		return "list-meetings";
 	}
