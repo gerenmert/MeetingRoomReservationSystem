5af90d63f1a9247037d179466d72d7fe78655004
mgeren <mgeren@mertgeren.sni.local>
2019-07-22 16:11:46 +0300

    edited

--- MeetingRoomReservationSystem/src/com/mertgeren/controller/MeetingController.java
+++ MeetingRoomReservationSystem/src/com/mertgeren/controller/MeetingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mertgeren.dao.MeetingDAO;
@@ -18,7 +19,7 @@ public class MeetingController {
 	@Autowired
 	private MeetingDAO meetingDAO;
 
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listMeetings(Model theModel) {
 		
 		// get meetings from the dao
